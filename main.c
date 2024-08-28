2a7c017c0417c8e69fb0d3c378d6fd6fc931b546
Sen Harald Hjort Inao <senharald07@gmail.com>
2024-08-28 22:13:51 +0200

    added saving

--- main.c
+++ main.c
@@ -32,6 +32,51 @@ int printBuffer(char** buffer, int lineCount, HANDLE hConsole) {
   return 0;
 }
 
+void printInfo(HANDLE hConsole, CONSOLE_SCREEN_BUFFER_INFO csbi, int saved, char filename[], char isSaved[]) {
+  ScreenSize size = getScreenSize(csbi);
+  COORD bottom = {0, size.rows-1};
+  SetConsoleCursorPosition(hConsole, bottom);
+  memset(isSaved, 0, sizeof(&isSaved));
+  if (saved) {
+    strcat(isSaved, "SAVED");
+  } else {
+    strcat(isSaved, "EDITED, ctrl+s to save");
+  }
+  printf("%s | %s", filename, isSaved);
+}
+
+void readFileToBuffer(char filename[], char ***buffer, int *lineCount, int *capacity) {
+  FILE *fpointer = fopen(filename, "r");
+  if (fpointer == NULL) {
+    fprintf(stderr, "Error opening file\n");
+  }
+
+  while (fgets((*buffer)[*lineCount], BUFFER_SIZE, fpointer) != NULL) {
+    size_t len = strlen((*buffer)[*lineCount]);
+    if (len > 0 && (*buffer)[*lineCount][len - 1] == '\n') {
+        (*buffer)[*lineCount][len - 1] = '\0';
+    }
+    (*lineCount)++;
+    if (*lineCount>=*capacity) {
+      *capacity+=1;
+      *buffer = realloc((*buffer), (*capacity) * sizeof(char*));
+      for (int i = *lineCount; i < *capacity; i++) {
+        (*buffer)[i] = (char*)malloc(BUFFER_SIZE);
+      }
+    }
+  }
+
+  fclose(fpointer);
+}
+
+void saveBuffer(char filename[], char **buffer, int lineCount) {
+  FILE *fpointer = fopen(filename, "w");
+  for (int i = 0; i < lineCount; i++) {
+    fprintf(fpointer, "%s\n", buffer[i]);
+  }
+  fclose(fpointer);
+}
+
 int main() {
   HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
   CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -40,6 +85,7 @@ int main() {
   char **buffer = NULL;
   int lineCount = 0;
   int capacity = INITIAL_CAPACITY;
+  char isSaved[30];
 
   buffer = malloc(capacity * sizeof(char*));
   for (int i = 0; i < capacity; i++) {
@@ -61,33 +107,16 @@ int main() {
   printf("Enter filename to edit: ");
   scanf("%s", filename);
 
-  FILE *fpointer = fopen(filename, "r");
-  if (fpointer == NULL) {
-    fprintf(stderr, "Error opening file\n");
-  }
-
-  while (fgets(buffer[lineCount], BUFFER_SIZE, fpointer) != NULL) {
-    size_t len = strlen(buffer[lineCount]);
-    if (len > 0 && buffer[lineCount][len - 1] == '\n') {
-        buffer[lineCount][len - 1] = '\0';
-    }
-    lineCount++;
-    if (lineCount>=capacity) {
-      capacity+=1;
-      buffer = realloc(buffer, capacity * sizeof(char*));
-      for (int i = lineCount; i < capacity; i++) {
-        buffer[i] = (char*)malloc(BUFFER_SIZE);
-      }
-    }
-  }
-
+  readFileToBuffer(filename, &buffer, &lineCount, &capacity);
 
   int c;
   boolean bufferChanged = 1;
+  boolean saved = 1;
   while (1) {
 
     if (bufferChanged) {
       printBuffer(buffer, lineCount, hConsole);
+      printInfo(hConsole, csbi, saved, filename, isSaved);
     }
     SetConsoleCursorPosition(hConsole, cursorPos); 
 
@@ -103,13 +132,13 @@ int main() {
     if (c == 0 || c == 224) { // Extended key prefix
       c = _getch();
       switch (c) {
-        case 75:
+        case 75: //LEFT arrow
           if (cursorPos.X == 0) {
             break;
           }
           cursorPos.X--;
           break;
-        case 72:
+        case 72: //UP arrow
           if (cursorPos.Y == 0) {
             break;
           }
@@ -118,13 +147,13 @@ int main() {
             cursorPos.X = strlen(buffer[cursorPos.Y]);
           }
           break;
-        case 77:
+        case 77: //RIGHT arrow
           if (cursorPos.X >= strlen(buffer[cursorPos.Y])) {
             break;
           }
           cursorPos.X++;
           break;
-        case 80:
+        case 80: //DOWN arrow
           if (cursorPos.Y == lineCount-1) {
             break;
           }
@@ -135,13 +164,14 @@ int main() {
           break;
       }
     } else {
-      if (c >= 32 && c <=126) {
+      saved = 0;
+      if (c >= 32 && c <=126) { //Any writable character
         memmove(&buffer[cursorPos.Y][cursorPos.X+1], &buffer[cursorPos.Y][cursorPos.X], strlen(&buffer[cursorPos.Y][cursorPos.X])+1);
         buffer[cursorPos.Y][cursorPos.X] = c;
         cursorPos.X++;
 
-      } else if (c == 8) {
-        if (cursorPos.X == 0 && cursorPos.Y > 0) {
+      } else if (c == 8) { //BACKSPACE
+        if (cursorPos.X == 0 && cursorPos.Y > 0) { //Go to previous line if backspace on first character
           int prevLineLen = strlen(buffer[cursorPos.Y - 1]);
           int currentLineLen = strlen(buffer[cursorPos.Y]);
 
@@ -160,7 +190,17 @@ int main() {
                   &buffer[cursorPos.Y][cursorPos.X], strlen(buffer[cursorPos.Y]));
           cursorPos.X--;
         } 
-      } else if (c == 13) {
+
+      } else if (c == 13) { //ENTER
+
+        if (lineCount == capacity) { //Realloc if needed
+          capacity+=1;
+          buffer = realloc(buffer, capacity * sizeof(char*));
+          for (int i = lineCount; i < capacity; i++) {
+            buffer[i] = (char*)malloc(BUFFER_SIZE);
+          }
+        }
+
         for (int i = lineCount; i > cursorPos.Y; i--) {
           strcpy(buffer[i], buffer[i - 1]);
         }
@@ -171,12 +211,14 @@ int main() {
         cursorPos.X = 0;
         cursorPos.Y++;
         lineCount++;
-      }
 
+      } else if (c == 19) { //CTRL+S
+        saveBuffer(filename, buffer, lineCount);
+        saved = 1;
+      }
       bufferChanged = 1;
     }
   }
-  fclose(fpointer);
 
   return 0;
 }
